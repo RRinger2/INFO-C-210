475dc104ce224d7a738d4323ee1d9f5d3e3e5487
clayvaughn <114845958+clayvaughn@users.noreply.github.com>
2024-09-12 04:44:44 -0400

    Assignment 1 part 3
    Let me know if there are issues with the code and I'll get to them asap

---
+++ Account.java
@@ -0,0 +1,85 @@
+import java.time.LocalDate; 
+
+public class Account {
+	private int id; 
+	private double balance; 
+	private double annualInterestRate; 
+	private LocalDate dateCreated; 
+	
+	
+	//default constructor 
+	public Account() {
+		this.id = 0; 
+		this.balance = 0.0; 
+	} // end default constructor 
+	
+	//non-default constructor 
+	public Account (int id, double balance, double annualInterestRate) {
+		this.id = id; 
+		this.balance = balance; 
+		this.annualInterestRate= annualInterestRate; 
+		
+	}//end non-default constructor 
+
+	//set/mutator methods 
+	public void setID(int id) {
+		this.id = id; 
+	} // end setID
+	
+	public void setAnnualInterestRate (double annualInterestRate) {
+		this.annualInterestRate = annualInterestRate; 
+	} // end setAnnualInterestRate
+	
+	//get/accessor methods 
+	public int getID(int id) {
+		return this.id = id; 
+	} // end getID
+	
+	public LocalDate getDateCreated (LocalDate dateCreated) {
+		return this.dateCreated = LocalDate.now(); 
+	} // end getDateCreated 
+	
+	public double getBalance(double balance) {
+		return this.balance = balance; 
+	} // end getBalance
+	
+	public double getAnnualInterestRate (double annualInterestRate) {
+		return this.annualInterestRate = annualInterestRate; 
+	} // end getAnnualInterestRate
+	
+	//Problem 1 # 9 
+	public double getMonthlyInterestRate() {
+		//need to write code to calculate annualInterestRate / 12 (remember 5% = .05) 
+		double monthlyInterestRate; 
+		return monthlyInterestRate;
+	} // end getMonthlyInterestRate
+	
+	// Problem 1 # 10
+	public double getMonthlyInterest() {
+		//need to write code to calculate monthlyInterestRate * balance (remember 5% = .05) 
+		double monthlyInterest;
+		return monthlyInterest;
+	} // end getMonthlyInterestRate
+	
+	// Problem 1 # 11 
+	public double withdraw (double withdrawAmount) {
+		// need to write code to withdraw a specified amount into the account 
+		double newBalance; 
+		return newBalance; 
+	} // end withdraw 
+	
+	// Problem 1 # 12 
+	public double deposit (double depositAmount) {
+		// need to write code to deposit a specified amount into the account 
+		double newBalance; 
+		return newBalance; 
+	} // end deposit 
+
+	public String toString() { 
+		String output = "Account ID: " + this.id; 
+		output+= "\nAccount Balance: " + this.balance; 
+		output+= "\nAnnual Intereset Rate: " + this.annualInterestRate + "\n"; 
+		
+		return output; 
+	}
+} //end class
