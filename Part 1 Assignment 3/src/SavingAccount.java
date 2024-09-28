7dc30b028dcb8d4c34d06d275e2d40c09f693bd1
Riley Ringer <2019rinri@gmail.com>
2024-09-27 20:26:50 -0400

    re commit

---
+++ Part 1 Assignment 3/src/SavingAccount.java
@@ -0,0 +1,53 @@
+// Declare Saving Account class inheriting form BankAccount
+public class SavingAccount extends Account {
+	
+	//Instance Variable 
+	protected double withdraw;
+	protected double deposit;
+	protected double InterestRate;
+	
+	//Constructor 
+	
+	public SavingAccount(String accountNumber, double balance) {
+		
+		// Call parent class constructor
+		
+		super(accountNumber, balance);
+	}
+	
+	//Override the withdraw method form the parent class
+	
+	public void withdraw(double amount) {
+		
+		if (getBalance () - amount < 100) {
+			
+			System.out.println("Minimum balance of $100 required!");
+			
+		} else {
+			
+			//Call the parent class withdraw mehtod
+			
+			super.withdraw(amount);
+		}
+	}
+	// Override the  deposit method from the parent class
+	public void deposit(double amount) {
+		
+		//this method should return the balance of the deposit method 
+		getBalance (amount);
+		
+		super.deposit(amount);
+				
+	}
+// I'm not sure if we should declare this method in SavingAccount class or not	
+	
+	protected double getAnnualInterestRate() {
+		if(getBalance() < 0) {
+			InterestRate = 0.0;
+		} else {
+			InterestRate = 0.05;
+		}
+	    return annualInterestRate;
+	}
+	
+}
